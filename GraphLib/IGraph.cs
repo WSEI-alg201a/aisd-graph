cf832d6fe3c15487dc098555b89e9b5dfdafe833
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2020-04-24 02:28:17 +0200

    Initial commit

---
+++ GraphLib/IGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace kmolenda.aisd.GraphLib
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="V">vertex - typ reprezętujący węzeł</typeparam>
+    /// <typeparam name="E">edge - typ reprezentujący krawędź</typeparam>
+    public interface IGraph<V, E> where E : IEdge<V>
+    {
+
+    }
+}
