b11571a49ae0f6d98c6c3590d0130e564827ec87
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2022-05-26 00:28:08 +0200

    Update IGraph.cs

--- GraphLib/IGraph.cs
+++ GraphLib/IGraph.cs
@@ -6,7 +6,7 @@ namespace kmolenda.aisd.GraphLib
     /// <summary>
     /// 
     /// </summary>
-    /// <typeparam name="V">vertex - typ reprezętujący węzeł</typeparam>
+    /// <typeparam name="V">vertex - typ reprezentujący węzeł</typeparam>
     /// <typeparam name="E">edge - typ reprezentujący krawędź</typeparam>
     public interface IGraph<V, E> where E : IEdge<V>
     {
