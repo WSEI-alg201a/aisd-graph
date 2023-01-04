f136baf9340b4b5d09d9d6d512571d93092daffd
kmolenda <kmolenda@wsei.edu.pl>
2023-01-05 00:36:06 +0100

    refactoring: ShortestPath

---
+++ GraphLib/AlgorithmsShortestPath.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+
+namespace kmolenda.aisd.GraphLib
+{
+    /// <summary>
+    /// Dostarcza metody rozszerzające interfejs `IGraph` o algorytmy szukania najkrótszej ścieżki
+    /// </summary>
+    public static class AlgorithmsShortestPath
+    {
+ 
+        // Wykorzystuje koncepcję BFS. Wychodząc od początkowego wierzchołka, zapamiętuje
+        // w słowniku `previous` jak dojść do każdego węzła. aby znaleźć najkrószą ścieżkę
+        // wyszukujemy poprzedni węzeł dla węzła docelowego i kontynuujemy przeglądanie
+        // wszystkich poprzednich węzłów, aż dotrzemy do węzła początkowego.
+        // Otrzymana ścieżka jest w kolejności odwrotnej, dlatego `Reverse();`
+
+        /// <summary>
+        /// Zwraca funkcję, która zwraca najkrótszą ścieżkę między wskazanymi węzłami (w sensie liczby krawędzi)
+        /// </summary>
+        /// <remarks>
+        /// Wykorzystuje dowolną implementację grafu, opartą na interfejsie `IGraph`.
+        /// Wykorzystuje koncepcję przeglądania BFS. Wychodząc od początkowego wierzchołka, zapamiętuje
+        /// w słowniku `previous` jak dojść do każdego węzła. Aby znaleźć najkrószą ścieżkę
+        /// wyszukujemy poprzedni węzeł dla węzła docelowego i kontynuujemy przeglądanie
+        /// wszystkich poprzednich węzłów, aż dotrzemy do węzła początkowego.
+        /// Otrzymana ścieżka jest w kolejności odwrotnej, dlatego `Reverse();`
+        /// </remarks>
+        /// <usage>var path = graph.ShortestPathFunc<int>(start: 1)(4); // dla węzłów typu `int`.</usage>
+        /// <param name="graph">graf, w dowolnej implementacji</param>
+        /// <param name="start">wierzchołek od którego rozpoczynane jest przeglądanie</param>
+        /// <typeparam name="V">vertex - typ wierzchołka</typeparam>
+        /// <returns>Funkcję, która dla określonego węzła zwraca ścieżkę</returns>
+        public static Func<V, IEnumerable<V>> ShortestPathFunc<V>(this IGraph<V, IEdge<V>> graph, V start)
+        {
+            var previous = new Dictionary<V, V>();
+
+            var queue = new Queue<V>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                foreach (var neighbour in graph.Neighbours(vertex))
+                {
+                    if (previous.ContainsKey(neighbour))
+                        continue;
+
+                    previous[neighbour] = vertex;
+                    queue.Enqueue(neighbour);
+                }
+            }
+
+            // funkcja lokalna, wewnętrzna, zwracająca najkrótszą ścieżkę
+            Func<V, IEnumerable<V>> shortestPath = v =>
+            {
+                var path = new List<V> { };
+
+                var current = v;
+                while (!current.Equals(start))
+                {
+                    path.Add(current);
+                    current = previous[current];
+                };
+
+                path.Add(start);
+                path.Reverse(); //można zakomentować, jeśli nie przeszkadza odwrotny porządek
+                                //zawsze można później odwrócić przechwycony wynik
+
+                return path;
+            };
+
+            return shortestPath;
+        } // koniec ShortestPathFunc
+
+        // Funkcja zwracająca sekwencję węzłów będacych najkrószą ścieżką (w sensie liczby krawędzi)
+        // między wskazanymi węzłami
+         public static IEnumerable<V> ShortestPath<V>(this IGraph<V, IEdge<V>> graph, V start, V end)
+            => graph.ShortestPathFunc<V>(start)(end);
+            
+    }
+}
