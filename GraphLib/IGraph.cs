9488d561b3656dc23a9bc4f5f282c56429113633
kmolenda <kmolenda@wsei.edu.pl>
2023-01-07 20:11:53 +0100

    correction of comments

--- GraphLib/IGraph.cs
+++ GraphLib/IGraph.cs
@@ -9,23 +9,41 @@ namespace kmolenda.aisd.GraphLib
     /// <typeparam name="E">edge - typ reprezentujący krawędź</typeparam>
     public interface IGraph<V, E> where E : IEdge<V>
     {
-        // dodaje węzeł do grafu
-        // jeśli węzeł już istnieje, nadpisuje
+
+        #region  === Constructors ===
+        // konstruktor bezparametrowy, tworzony jest pusty graf
+
+        // konstruktor z parametrami `vertices` i `edges`, tworzony jest graf 
+        // z podanymi węzłami `IEnumerable<V>` i krawędziami `IEnumerable<IEdge<V>>`
+        # endregion === Constructors ===
+
+        #region === Vertices ===
+        // dodaje węzeł do grafu, zwraca `true`, jeśli węzeł został dodany
+        // jeśli węzeł już istnieje, nadpisuje, zwraca `false` (nie zgłasza wyjątku)
         bool AddVertex(V vertex);
+
+        // usuwa węzeł z grafu, zwraca `true`, jeśli węzeł został usunięty
+        // jeśli węzeł nie istnieje, zwraca `false` (nie zgłasza wyjątku)
+        bool RemoveVertex(V vertex);
 
         // sprawdza, czy węzeł jest w grafie
         bool ContainsVertex(V vertex);
 
-        // zwraca kolekcję węzłów połączonych z podanym
+        // zwraca iterator/kolekcję węzłów połączonych z podanym
         IEnumerable<V> Neighbours(V vertex);
 
-        // zwraca kolekcję wszystkich węzłów
+        // zwraca iterator/kolekcję wszystkich węzłów
         IEnumerable<V> Vertices { get; }
+        #endregion === Vertices ===
 
-        // dodaje krawędź do grafu
+        #region === Edges ===
+        // dodaje krawędź do grafu, zwraca `true`, jeśli krawędź została dodana
+        // jeśli krawędź już istnieje, nadpisuje, zwraca `false` (nie zgłasza wyjątku)
+        // jeśli którykolwiek z wierzchołków nie istnieje, zwraca `false` (nie zgłasza wyjątku)
         bool AddEdge(E edge);
 
-        // zwraca kolekcję wszystkich krawędzi
+        // zwraca iterator/kolekcję wszystkich krawędzi
         IEnumerable< E > Edges {get;}
+        #endregion === Edges ===
     }
 }
